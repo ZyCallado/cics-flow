cda2d80e7999c66e80179038fed9fa3f604eb83b
Zyrus C. Velasco <zyrus.velasco@neu.edu.ph>
2026-03-09 01:37:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3562924685-77875",
+  "appId": "1:338235292831:web:bf489ba3ad226adf39b6aa",
+  "apiKey": "AIzaSyDqUQPcBdPPEuNkWiufyV6-x40wgTnhjVo",
+  "authDomain": "studio-3562924685-77875.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "338235292831"
+};
