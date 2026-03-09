961710efa9c91ddb1186c71622bda1c5656f127a
Zyrus C. Velasco <zyrus.velasco@neu.edu.ph>
2026-03-09 10:56:11 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:338235292831:web:bf489ba3ad226adf39b6aa",
   "apiKey": "AIzaSyDqUQPcBdPPEuNkWiufyV6-x40wgTnhjVo",
   "authDomain": "studio-3562924685-77875.firebaseapp.com",
+  "storageBucket": "studio-3562924685-77875.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "338235292831"
 };
