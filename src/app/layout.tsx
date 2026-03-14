210ceb784eec8411f076f77e01ae191841611ac4
Zyrus C. Velasco <zyvel0@gmail.com>
2026-03-14 17:52:32 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 
-import type {Metadata} from 'next'
+import type {Metadata} from 'next';
 import './globals.css';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 import { Toaster } from '@/components/ui/toaster';
